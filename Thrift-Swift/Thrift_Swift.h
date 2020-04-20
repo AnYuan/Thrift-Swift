0757b6e950fcff1feb923455b3b34ea7a7b5a9d9
Anyuan Dong <donganyuan1010@gmail.com>
2020-04-20 14:32:52 +0800

    add project

---
+++ Thrift-Swift/Thrift_Swift.h
@@ -0,0 +1,19 @@
+//
+//  Thrift_Swift.h
+//  Thrift-Swift
+//
+//  Created by Dong, Anyuan (133) on 2020/4/20.
+//  Copyright © 2020 Anyuan Dong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Thrift_Swift.
+FOUNDATION_EXPORT double Thrift_SwiftVersionNumber;
+
+//! Project version string for Thrift_Swift.
+FOUNDATION_EXPORT const unsigned char Thrift_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Thrift_Swift/PublicHeader.h>
+
+
